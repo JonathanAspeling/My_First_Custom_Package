6c45cefc9f71745cd031cab436b3650413f34868
Jonathan Awesome <jbaspeling@gmail.com>
2019-03-19 07:45:50 +0200

    more changes

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='edsajonathan',
+    name='mypackage',
     version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
-    author='<Your Name>',
-    author_email='<Your Email>'
+    url='https://github.com/faulty-it/mypackage',
+    author='jonathan',
+    author_email='jbaspeling@gmail.com'
 )
