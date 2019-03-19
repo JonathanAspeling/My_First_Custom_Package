ded603c2cb14a0ba4446c26d5efbf8c05a540f2c
Jonathan Awesome <jbaspeling@gmail.com>
2019-03-19 07:16:17 +0200

    removed problematic merge_sort

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -10,36 +10,6 @@ def bubble_sort(items):
     return items        
 
 
-def merge(left, right):
-    """Merge sort merging function."""
-
-    left_index, right_index = 0, 0
-    result = []
-    while left_index < len(left) and right_index < len(right):
-        if left[left_index] < right[right_index]:
-            result.append(left[left_index])
-            left_index += 1
-        else:
-            result.append(right[right_index])
-            right_index += 1
-
-    result += left[left_index:]
-    result += right[right_index:]
-    return result
-
-def merge_sort(items):
-
-    '''Return array of items, sorted in ascending order'''
-    if len(items) <= 1:
-        return items
-
-    items_half_index = len(items)//2
-    items_left = merge_sort(items[:items_half_index])
-    items_right = merge_sort(items[items_half_index:])
-
-    return merge(items_left,items_right)
-
-
 def quick_sort(items):
 
     '''Return array of items, sorted in ascending order'''
